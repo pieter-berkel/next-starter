7c77821371303222a5bf328316d7f2b8c4552e63
Pieter Berkel <pieterberkel@msn.com>
2024-12-25 15:36:28 +0100

    refactor db utils to use timestamp type for createdAt, updatedAt, and deletedAt fields

--- db/utils.ts
+++ db/utils.ts
@@ -1,18 +1,12 @@
 import { nanoid } from "@/lib/nanoid";
-import { sql } from "drizzle-orm";
-import { datetime, varchar } from "drizzle-orm/mysql-core";
+import { timestamp, varchar } from "drizzle-orm/mysql-core";
 
 export const id = varchar("id", { length: 16 })
   .primaryKey()
   .$defaultFn(() => nanoid());
 
-export const createdAt = datetime("created_at")
-  .notNull()
-  .default(sql`NOW()`);
+export const createdAt = timestamp("created_at").notNull().defaultNow();
 
-export const updatedAt = datetime("updated_at")
-  .notNull()
-  .default(sql`NOW()`)
-  .$onUpdate(() => sql`NOW()`);
+export const updatedAt = timestamp("updated_at").notNull().defaultNow().onUpdateNow();
 
-export const deletedAt = datetime("deleted_at");
+export const deletedAt = timestamp("deleted_at");
